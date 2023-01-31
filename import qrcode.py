c9b3b34d8772e545fed9307684d583dc2b46a935
Muhammed Samet Akgül <93324656+muhammedsametakgul@users.noreply.github.com>
2023-01-31 13:00:54 +0300

    Added random names for QR Codes

--- import qrcode.py
+++ import qrcode.py
@@ -1,15 +1,16 @@
 import qrcode
 import image
+import random
 
 qr=qrcode.QRCode(
     version= 15,
-    box_size=10,
+    box_size=8,
     border= 5
 )
-
+name=random.randint(0,100000)
 data =input()
 
 qr.add_data(data)
 qr.make(fit=True)
 img =qr.make_image(fill="black",back_color="white")
-img.save("test.png")
+img.save(str(name) +".png")
